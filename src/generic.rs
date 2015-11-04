62db7fdb0fc2086bf4c1ed31e8a605ef84839012
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2015-11-04 09:40:31 +0100

    BTreeMap for Obj, Ord

--- src/generic.rs
+++ src/generic.rs
@@ -1,14 +1,15 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::hash::{Hash, Hasher};
 use std::mem::transmute;
 use std::fmt;
+use std::cmp::Ordering;
 
 use serde::bytes::ByteBuf;
 use serde::{Serialize, Serializer, Deserialize, Deserializer};
 use serde::de::{Visitor, SeqVisitor, MapVisitor, Error};
 
 #[allow(dead_code)]
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Clone)]
 pub enum Obj {
     Null,
     Bool(bool),
@@ -18,7 +19,23 @@ pub enum Obj {
     Str(String),
     Bin(ByteBuf),
     List(Vec<Obj>),
-    Map(HashMap<Obj, Obj>)
+    Map(BTreeMap<Obj, Obj>)
+}
+
+impl Obj {
+    fn type_num(&self) -> u8 {
+        match self {
+            &Obj::Null => 0,
+            &Obj::Bool(_) => 1,
+            &Obj::Unsigned(_) => 2,
+            &Obj::Signed(_) => 3,
+            &Obj::Float(_) => 4,
+            &Obj::Str(_) => 5,
+            &Obj::Bin(_) => 6,
+            &Obj::List(_) => 7,
+            &Obj::Map(_) => 8,
+        }
+    }
 }
 
 impl Default for Obj {
@@ -28,49 +45,102 @@ impl Default for Obj {
     }
 }
 
+impl PartialEq for Obj {
+    fn eq(&self, other: &Self) -> bool {
+        match self {
+            &Obj::Null => if let &Obj::Null = other { true } else { false },
+            &Obj::Bool(val) => if let &Obj::Bool(oval) = other { val == oval } else { false },
+            &Obj::Unsigned(val) => if let &Obj::Unsigned(oval) = other { val == oval } else { false },
+            &Obj::Signed(val) => if let &Obj::Signed(oval) = other { val == oval } else { false },
+            &Obj::Float(val) => if let &Obj::Float(oval) = other { ! (val != oval) } else { false },
+            &Obj::Str(ref val) => if let &Obj::Str(ref oval) = other { val == oval } else { false },
+            &Obj::Bin(ref val) => if let &Obj::Bin(ref oval) = other { val == oval } else { false },
+            &Obj::List(ref val) => if let &Obj::List(ref oval) = other { val == oval } else { false },
+            &Obj::Map(ref val) => if let &Obj::Map(ref oval) = other { val == oval } else { false },
+        }
+    }
+}
+
 impl Eq for Obj {}
+
+impl PartialOrd for Obj {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Obj {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let stype = self.type_num();
+        let otype = other.type_num();
+        if stype != otype {
+            return stype.cmp(&otype);
+        }
+        match self {
+            &Obj::Null => Ordering::Equal,
+            &Obj::Bool(val) => if let &Obj::Bool(oval) = other {
+                val.cmp(&oval)
+            } else {
+                unreachable!()
+            },
+            &Obj::Unsigned(val) => if let &Obj::Unsigned(oval) = other {
+                val.cmp(&oval)
+            } else {
+                unreachable!()
+            },
+            &Obj::Signed(val) => if let &Obj::Signed(oval) = other {
+                val.cmp(&oval)
+            } else {
+                unreachable!()
+            },
+            &Obj::Float(val) => if let &Obj::Float(oval) = other {
+                if !val.is_nan() && !oval.is_nan() {
+                    val.partial_cmp(&oval).unwrap()
+                } else if val.is_nan() {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                }
+            } else {
+                unreachable!()
+            },
+            &Obj::Str(ref val) => if let &Obj::Str(ref oval) = other {
+                val.cmp(&oval)
+            } else {
+                unreachable!()
+            },
+            &Obj::Bin(ref val) => if let &Obj::Bin(ref oval) = other {
+                val.cmp(&oval)
+            } else {
+                unreachable!()
+            },
+            &Obj::List(ref val) => if let &Obj::List(ref oval) = other {
+                val.cmp(&oval)
+            } else {
+                unreachable!()
+            },
+            &Obj::Map(ref val) => if let &Obj::Map(ref oval) = other {
+                val.cmp(&oval)
+            } else {
+                unreachable!()
+            },
+        }
+    }
+}
 
 impl Hash for Obj {
     fn hash<H>(&self, state: &mut H) where H: Hasher {
+        state.write_u8(self.type_num());
         match self {
-            &Obj::Null => state.write_u8(0),
-            &Obj::Bool(val) => {
-                state.write_u8(1);
-                state.write_u8(if val { 1 } else { 0 });
-            },
-            &Obj::Unsigned(val) => {
-                state.write_u8(2);
-                state.write_u64(val);
-            },
-            &Obj::Signed(val) => {
-                state.write_u8(3);
-                state.write_i64(val);
-            },
-            &Obj::Float(val) => {
-                state.write_u8(4);
-                state.write_u64(unsafe { transmute(val) });
-            },
-            &Obj::Str(ref val) => {
-                state.write_u8(5);
-                val.hash(state);
-            },
-            &Obj::Bin(ref val) => {
-                state.write_u8(6);
-                val.hash(state);
-            },
-            &Obj::List(ref val) => {
-                state.write_u8(7);
-                for value in val {
-                    value.hash(state);
-                }
-            },
-            &Obj::Map(ref val) => {
-                state.write_u8(8);
-                for (key, value) in val {
-                    key.hash(state);
-                    value.hash(state);
-                }
-            }
+            &Obj::Null => (),
+            &Obj::Bool(val) => state.write_u8(if val { 1 } else { 0 }),
+            &Obj::Unsigned(val) => state.write_u64(val),
+            &Obj::Signed(val) => state.write_i64(val),
+            &Obj::Float(val) => state.write_u64(unsafe { transmute(val) }),
+            &Obj::Str(ref val) => val.hash(state),
+            &Obj::Bin(ref val) => val.hash(state),
+            &Obj::List(ref val) => val.hash(state),
+            &Obj::Map(ref val) => val.hash(state),
         }
     }
 }
@@ -161,7 +231,7 @@ impl Visitor for GenericVisitor {
 
     #[inline]
     fn visit_map<V: MapVisitor>(&mut self, mut visitor: V) -> Result<Self::Value, V::Error> {
-        let mut map = HashMap::with_capacity(visitor.size_hint().0);
+        let mut map = BTreeMap::new();
         while let Some((key, value)) = try!(visitor.visit()) {
             map.insert(key, value);
         }
