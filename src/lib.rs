717b00a6c656d72d77bf8667dc533ed5cf139843
Dennis Schwerdel <schwerdel@googlemail.com>
2015-11-04 18:13:33 +0100

    Fixed docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! This crate provides some utility macros and types to be used with Serde.
 //! It currently includes:
 //!
-//! * [`Obj`](struct.Obj.html) - A generic object that can hold any value deserialized via Serde.
+//! * [`Obj`](enum.Obj.html) - A generic object that can hold any value deserialized via Serde.
 //!
 //! * [`serde_impl!`](macro.serde_impl!.html#using-the-macro) - A macro for implementing (de-)serialization
 //!   via serde in common cases.
